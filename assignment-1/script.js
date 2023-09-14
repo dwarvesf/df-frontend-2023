a0324a18dd69c321ccf13a0a2803beed2c449a31
victhangnguyen <victhangnguyen@outlook.com>
2023-09-15 06:09:52 +0700

    setup Book Store Management Application

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,7 @@
 // Your JS code goes here
+const input = document.getElementById("ipt-search");
+console.log("input", input);
+
+input.addEventListener("input", () => {
+  input.setAttribute("value", input.value);
+});
