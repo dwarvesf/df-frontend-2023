969c363773f423177725f45ba5a891976cb5d71d
Nhut Huynh <nhuthuynh.steve@setel.com>
2023-09-16 15:14:35 +0700

    feat: assignment 1 - FE23 - Implemented the basic HTML file - Implemented the basic CSS file - Implemented JS file with 2 features -- Create a new item -- Remove an item

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,47 @@
-// Your JS code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const addItemBtn = document.getElementById("addItemBtn");
+    const itemNameInput = document.getElementById("itemName");
+    const itemDescriptionInput = document.getElementById("itemDescription");
+    const itemTopicInput = document.getElementById("itemTopic");
+    const itemList = document.getElementById("itemList");
+
+    addItemBtn.addEventListener("click", function() {
+        const itemName = itemNameInput.value;
+        const itemDescription = itemDescriptionInput.value;
+        const itemTopic = itemTopicInput.value;
+
+        if (itemName.trim() !== "") {
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = `
+                 <td>${itemName}</td>
+                 <td>${itemDescription}</td>
+                 <td>${itemTopic}</td>
+                 <td><button class="delete-btn">Delete</button></td>
+            `;
+
+            itemList.appendChild(newRow);
+
+            itemNameInput.value = "";
+            itemDescriptionInput.value = "";
+        
+            const deleteButton = newRow.querySelector(".delete-btn");
+            deleteButton.addEventListener("click", function () {
+                const confirmationModal = document.getElementById("confirmationModal");
+                confirmationModal.style.display = "block";
+
+                const confirmDeleteButton = document.getElementById("confirmDelete");
+                confirmDeleteButton.addEventListener("click", function () {
+                    newRow.remove();
+                    confirmationModal.style.display = "none";
+                });
+            });
+
+            const closeButtons = document.getElementsByClassName("close");
+            for (const closeButton of closeButtons) {
+                closeButton.addEventListener("click", function () {
+                    confirmationModal.style.display = "none"; // Close the modal
+                });
+            }
+        }
+    });
+});
