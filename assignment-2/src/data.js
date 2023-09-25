f373bc37605a85aa8a45182892efa666e7cd0e2a
Nguyễn Duy Trung <duytrung.ng1@gmail.com>
2023-09-25 23:59:07 +0700

    Add files via upload

---
+++ assignment-2/src/data.js
@@ -0,0 +1,39 @@
+const allBooks = [
+  {
+    name: "Refactoring",
+    author: "Martin Fowler",
+    topic: "Programming",
+  },
+  {
+    name: "Design Data-Intensive Applications",
+    author: "Martin Kleppman",
+    topic: "Database",
+  },
+  {
+    name: "The Phoenix Project",
+    author: "Gene Kim",
+    topic: "Devops",
+  },
+  {
+    name: "Java: The Good Part",
+    author: "Douglas Crockford",
+    topic: "Frontend",
+  },
+  {
+    name: "Node.js Design Patterns",
+    author: "Mario Casciaro",
+    topic: "Backend",
+  },
+  {
+    name: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    author: "Robert C. Martin",
+    topic: "Programming",
+  },
+  {
+    name: "Database Systems: The Complete Book",
+    author: "Hector Garcia-Molina",
+    topic: "Database",
+  },
+];
+
+export default allBooks;
