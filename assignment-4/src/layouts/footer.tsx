84545dbaf62814482f3c1f28af433579fa5a6c2c
Nguyễn Duy Trung <duytrung.ng1@gmail.com>
2023-10-04 23:46:05 +0700

    Add files via upload

---
+++ assignment-4/src/layouts/footer.tsx
@@ -0,0 +1,14 @@
+export default function MainFooter() {
+  return (
+    <footer>
+      Made by{' '}
+      <span>
+        <a href="https://github.com/iTeddy1">@Teddy</a>
+      </span>{' '}
+      and
+      <span>
+        <a href="https://github.com/dwarvesf">@Dwarves</a>
+      </span>
+    </footer>
+  )
+}
