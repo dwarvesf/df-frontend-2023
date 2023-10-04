44e798c7f0fd217d4949c0c90a9f7320cead23d7
Nguyễn Duy Trung <duytrung.ng1@gmail.com>
2023-10-05 02:06:47 +0700

    change layouts

--- assignment-4/src/app/page.tsx
+++ assignment-4/src/app/page.tsx
@@ -4,9 +4,5 @@ import React from 'react'
 import MainLayout from '../layouts/MainLayouts'
 
 export default function Home() {
-  return (
-    <div>
-      <MainLayout />
-    </div>
-  )
+  return <MainLayout />
 }
