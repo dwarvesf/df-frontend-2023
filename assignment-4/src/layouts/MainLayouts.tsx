77f9cacbfdd1eadd03d6ca9955b9f3470ee27a63
Nguyễn Duy Trung <duytrung.ng1@gmail.com>
2023-10-04 23:45:36 +0700

    Update MainLayouts.tsx

--- assignment-4/src/layouts/MainLayouts.tsx
+++ assignment-4/src/layouts/MainLayouts.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import MainBody from './body'
 import MainHeader from './header'
+import MainFooter from './footer'
 
 function MainLayout() {
   return (
     <div className="">
       <MainHeader />
       <MainBody />
-      {/* <MainFooter /> */}
+      <MainFooter />
     </div>
   )
 }
