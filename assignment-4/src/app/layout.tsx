3c08742ed07e93ce2ec5bad642182c85ea481602
Nguyễn Duy Trung <duytrung.ng1@gmail.com>
2023-10-05 22:57:58 +0700

    fix

--- assignment-4/src/app/layout.tsx
+++ assignment-4/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from '../components/Theme/theme-provider'
+import { ThemeProvider } from '../components/theme/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="h-full dark:h-full bg-slate-50  dark:bg-slate-900" >
+      <body
+        className={`${inter.className} h-full dark:h-full bg-slate-50 dark:bg-slate-900`}
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}{' '}
         </ThemeProvider>
